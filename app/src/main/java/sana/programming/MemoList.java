63a975e4e8cbae7def110239241fa031014e153b
sana <sana.programming.04@gmail.com>
2023-01-26 21:36:02 +0900

    new class

---
+++ app/src/main/java/sana/programming/MemoList.java
@@ -0,0 +1,14 @@
+package sana.programming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_list);
+    }
+}
